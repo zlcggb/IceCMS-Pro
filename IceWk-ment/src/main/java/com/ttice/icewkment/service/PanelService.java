85117f231b2dbbfd05ac7cb1f3f4cbe1cac53645
Thecosy <wufaquanshidebeiliang@gmail.com>
2024-02-20 03:33:53 +0800

    rename

--- IceWk-ment/src/main/java/com/ttice/icewkment/service/PanelService.java
+++
@@ -1,8 +0,0 @@
-package com.ttice.icewkment.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ttice.icewkment.entity.Panel;
-
-public interface PanelService extends IService<Panel> {
-  Panel SearchPanelInfo();
-}
