4865b8526ae66c45c440c62e5a8e1c93fa510845
Thecosy <wufaquanshidebeiliang@gmail.com>
2024-02-21 02:23:58 +0800

    view

--- IceCMS-ment/src/main/java/com/ttice/icewkment/configuration/ShiroConfig.java
+++ IceCMS-ment/src/main/java/com/ttice/icewkment/configuration/ShiroConfig.java
@@ -18,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
